d672a98b66a83842a339f6c3ab9372e7d62bc1c7
Max <durnenko.maxim@gmail.com>
2017-11-15 15:25:09 +0200

    Header

---
+++ app/js/main.js
@@ -0,0 +1,42 @@
+
+$(".buttom_menu").click(function(){
+   	$(this).toggleClass("active");
+  	$(this).toggleClass("close"); 
+   	$(".top_menu").fadeToggle(600);
+  	$(".list").fadeToggle(0);
+  	if($(".buttom_menu").hasClass("active")){
+  		$(".list").wrapAll("<nav class='top_menu'></nav>");
+  	} else {
+  		$(".list").unwrap();
+  	}
+  });
+
+function listDisplay() {
+	if($(window).width() > 814) {
+		$(".list").css("display","block");	
+	} else {
+		$(".list").css("display","none");
+
+	}
+};
+
+listDisplay();
+
+$(window).resize(function(){
+	listDisplay();
+})
+
+
+
+				
+
+ 				
+
+		
+
+	
+
+
+
+
+
